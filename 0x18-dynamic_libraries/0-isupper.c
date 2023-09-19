220234f11a2aba991aabc03943dd6ee54ea30abc
Prosper0200 <agarreyiruoghene@gmail.com>
2023-09-19 13:53:59 -0700

    dynamic

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - this function checks for uppercase  characters
+ * @c: This is the character to be tested
+ * Return: 1 if c is uppercase, otherwise  0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
